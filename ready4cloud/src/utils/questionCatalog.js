7d9c354f693a26347d5335223a32456792fa7eba
Mohamed Elgendy <memoelgendy40@gmail.com>
2023-08-24 10:30:07 +0200

    reduced nr. of modules

--- ready4cloud/src/utils/questionCatalog.js
+++ ready4cloud/src/utils/questionCatalog.js
@@ -49,20 +49,10 @@ export const questionCatalog = [
     topic: "Betriebszeiten, Service-Level und Verfügbarkeit",
     subTopic: [
       {
-        name: "Betriebszeiten",
+        name: "Rahmenbedingungen",
         questions: [
           "Können die Anforderungen an Betriebszeiten des Geschäftspartners durch die angebotenen Betriebszeiten des Cloudanbieters erfüllt werden? (z.B. 24/7 Betrieb)",
-        ],
-      },
-      {
-        name: "Servicezeiten",
-        questions: [
           "Können die Anforderungen an Servicezeiten des Geschäftspartners durch die angebotenen Servicezeiten des Cloudanbieters erfüllt werden? (z.B. 24/7 Servicesupport) ",
-        ],
-      },
-      {
-        name: "Reaktionszeiten",
-        questions: [
           "Können die Anforderungen an Reaktionszeiten des Geschäftspartners durch die angebotenen Reaktionszeiten des Cloudanbieters erfüllt werden? (z.B. 24/7 Support) ",
         ],
       },
@@ -76,12 +66,6 @@ export const questionCatalog = [
         name: "Service Verfügbarkeit",
         questions: [
           "Können individuelle - auf die Bedürfnisse des Kunden/Geschäftspartner angepasste Verfügbarkeiten des Service mit dem Cloudanbieter definiert werden?",
-        ],
-      },
-      {
-        name: "Datenmanagement  ",
-        questions: [
-          "Bietet der Cloud-Service Datenredundanz an mehreren geografischen Standorten?",
         ],
       },
       {
@@ -117,11 +101,8 @@ export const questionCatalog = [
         name: "Ressourcenmanagement",
         questions: [
           "Gibt es Tools zur Überwachung der Ressourcennutzung und Kosten?",
+          "Bietet der CSP unterschiedlische Kostenmodelle an?",
         ],
-      },
-      {
-        name: "Ressourcenmanagement ",
-        questions: ["Bietet der CSP unterschiedlische Kostenmodelle an?"],
       },
     ],
   },
@@ -132,43 +113,15 @@ export const questionCatalog = [
         name: "Integrationsfähigkeiten",
         questions: [
           "Kann eine Intregation zwischen baseIT und einem fremden Ticketsystem aufgebaut werden?",
+          "Gibt es ein Zugriff auf ein fremdes Ticketsystem?",
         ],
-      },
-      {
-        name: "Integrationsfähigkeiten ",
-        questions: ["Gibt es ein Zugriff auf ein fremdes Ticketsystem?"],
       },
       {
         name: "Datenmanagement",
-        questions: ["Unterstützt der CSP offene Standards und APIs?"],
-      },
-      {
-        name: "Datenmanagement ",
         questions: [
+          "Unterstützt der CSP offene Standards und APIs?",
           "Gibt es die Möglichkeit die Daten zurück On-Premise vollständig herunterzuladen?",
-        ],
-      },
-    ],
-  },
-  {
-    topic: "Sonstiges",
-    subTopic: [
-      {
-        name: "Kommunikation und Transparenz",
-        questions: [
-          "Kann ein Servicemanager Einsicht in Changes beim Cloudanbieter haben?",
-        ],
-      },
-      {
-        name: "Kommunikation und Transparenz ",
-        questions: [
-          "Werden geplante und ungeplante Wartungsarbeiten auf Seiten des Cloudanbieter an Servicemanager kommuniziert?",
-        ],
-      },
-      {
-        name: "Sicherheitsproaktivität",
-        questions: [
-          "Gibt es vom Cloudanbieter die Möglichkeit proaktiv auf Sicherheitsschwachstellen in der Software/Applikation hingewiesen zu werden?",
+          "Bietet der Cloud-Service Datenredundanz an mehreren geografischen Standorten?",
         ],
       },
     ],
@@ -180,15 +133,9 @@ export const questionCatalog = [
         name: "Datenschutz",
         questions: [
           "Erfüllt der Cloud Service Provider (CSP) die Anforderungen an den Datenschutz?",
+          "Werden Daten während der Übertragung verschlüsselt?",
+          "Werden heiße/kalte Daten verschlüsselt?",
         ],
-      },
-      {
-        name: "Datenschutz ",
-        questions: ["Werden Daten während der Übertragung verschlüsselt?"],
-      },
-      {
-        name: " Datenschutz",
-        questions: ["Werden heiße/kalte Daten verschlüsselt?"],
       },
       {
         name: "Zugriffsmanagement",
@@ -198,29 +145,27 @@ export const questionCatalog = [
         name: "Sicherheit und Risikomanagement",
         questions: [
           "Gibt es eine klare Richtlinie für den Umgang mit Sicherheitsvorfällen?",
-        ],
-      },
-      {
-        name: "Sicherheit und Risikomanagement ",
-        questions: [
           "Bietet der Service standardmäßig Multi-Faktor-Authentifizierung an?",
-        ],
-      },
-      {
-        name: " Sicherheit und Risikomanagement",
-        questions: [
           "Werden regelmäßige unabhängige Sicherheitsaudits durch den CSP durchgeführt?",
         ],
       },
     ],
   },
   {
-    topic: "Compliance und Regelungen",
+    topic: "Sonstiges",
     subTopic: [
       {
-        name: "Kommunikation und Transparenz  ",
+        name: "Kommunikation und Transparenz",
         questions: [
+          "Kann ein Servicemanager Einsicht in Changes beim Cloudanbieter haben?",
+          "Werden geplante und ungeplante Wartungsarbeiten auf Seiten des Cloudanbieter an Servicemanager kommuniziert?",
           "Werden die aktuellen Regelung zur Arbeitsnehmerüberlassung (ANÜ) eingehalten?",
+        ],
+      },
+      {
+        name: "Sicherheitsproaktivität",
+        questions: [
+          "Gibt es vom Cloudanbieter die Möglichkeit proaktiv auf Sicherheitsschwachstellen in der Software/Applikation hingewiesen zu werden?",
         ],
       },
     ],
